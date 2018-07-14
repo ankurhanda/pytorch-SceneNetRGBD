5ec183c88d2ef3dac21412e1e13cc347fbfc528a
Ankur Handa <ahanda@nvidia.com>
2018-07-14 12:41:31 -0700

    chaning the size of d tensor
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- unet_rgbd_pytorch.py
+++ unet_rgbd_pytorch.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.serialization import load_lua
-lua_unet = load_lua('../SCENENET_RESULTS_FOLDER_RERUN/NYUv2_TABLE/SCENENET_RGBD_EPOCH_10/converted_model.t7')
+#lua_unet = load_lua('../SCENENET_RESULTS_FOLDER_RERUN/NYUv2_TABLE/SCENENET_RGBD_EPOCH_10/converted_model.t7')
 
 import numpy as np
 
