b479016a8c863c0a094ffa2f03974bb0db7883ce
Ankur Handa <ahanda@nvidia.com>
2018-07-14 19:17:30 -0700

    trying all layers
    Signed-off-by: Ankur Handa <ahanda@nvidia.com>

--- unet_rgbd_pytorch.py
+++ unet_rgbd_pytorch.py
@@ -134,115 +134,52 @@ class UNetRGBD(nn.Module):
         self.conv1024_512 = nn.Conv2d(1024, 512, 3, 1, 1)
         self.up = nn.Upsample(scale_factor=2, mode='nearest')
 
-
-        '''
-        self.conv64_128 = nn.Conv2d(64, 128, 3, 1, 1)
-        self.bn64_128 = nn.BatchNorm2d(128, track_running_stats=True)
-        self.bn64_128.training = False
-
-        self.relu64_128 = nn.ReLU(inplace=True)
-
-        self.conv128_128 = nn.Conv2d(128, 128, 3, 1, 1)
-        self.bn128_128 = nn.BatchNorm2d(128, track_running_stats=True)
-        self.bn128_128.training = False
-
-        self.relu128_128 = nn.ReLU(inplace=True)
-        self.pool_128 = nn.MaxPool2d(2, 2)
-
-
-
-
-        self.conv128_256 = nn.Conv2d(128, 256, 3, 1, 1)
-        self.bn128_256 = nn.BatchNorm2d(256, track_running_stats=True)
-        self.bn128_256.training = False
-
-        self.relu128_256 = nn.ReLU(inplace=True)
-
-        self.conv256_256 = nn.Conv2d(256, 256, 3, 1, 1)
-        self.bn256_256 = nn.BatchNorm2d(256, track_running_stats=True)
-        self.bn256_256.training = False
-
-        self.relu256_256 = nn.ReLU(inplace=True)
-        self.pool_256 = nn.MaxPool2d(2, 2)
-
-
-
-        self.conv256_512 = nn.Conv2d(256, 512, 3, 1, 1)
-        self.bn256_512 = nn.BatchNorm2d(512, track_running_stats=True)
-        self.bn256_512.training = False
-
-        self.relu256_512 = nn.ReLU(inplace=True)
-
-        self.conv512_512 = nn.Conv2d(512, 512, 3, 1, 1)
-        self.bn512_512 = nn.BatchNorm2d(512, track_running_stats=True)
-        self.bn512_512.training = False
-
+        self.conv_512_512 = nn.Conv2d(512, 512, 3, 1, 1)
+        self.bn_512_512 = nn.BatchNorm2d(512, track_running_stats=True)
+        self.bn_512_512.training = False
         self.relu512_512 = nn.ReLU(inplace=True)
 
-        self.pool_512 = nn.MaxPool2d(2, 2)
-        self.conv512_1024 = nn.Conv2d(512, 1024, 3, 1, 1)
-        self.conv1024_512 = nn.Conv2d(1024, 512, 3, 1, 1)
-        self.up512 = nn.Upsample(scale_factor=2, mode='nearest')
-
-        # self.concat1024 = torch.cat([self.up512, self.relu512_512], dim=1)
-
-        self.conv1024_512_u = nn.Conv2d(1024, 512, 3, 1, 1)
-        self.bn1024_512_u = nn.BatchNorm2d(512, track_running_stats=True)
-        self.bn1024_512_u.training = False
-
-        self.relu1024_512_u = nn.ReLU(inplace=False)
-        self.conv512_256_u = nn.Conv2d(512, 256, 3, 1, 1)
-
-        self.bn512_256_u = nn.BatchNorm2d(256, track_running_stats=True)
-        self.bn512_256_u.training = False
-
-        self.relu512_256_u = nn.ReLU(inplace=True)
-        self.up256 = nn.Upsample(scale_factor=2, mode='nearest')
-
-        # self.concat512 = torch.cat([self.relu256_256, self.up256])
-
-        self.conv512_256_catu = nn.Conv2d(512, 256, 3, 1, 1)
-        self.bn512_256_catu = nn.BatchNorm2d(256, track_running_stats=True)
-        self.bn512_256_catu.training = False
-
-        self.relu512_256_catu = nn.ReLU(inplace=True)
-        self.conv256_128_catu = nn.Conv2d(256, 128, 3, 1, 1)
-        self.bn256_128_catu = nn.BatchNorm2d(128, track_running_stats=True)
-        self.bn256_128_catu.training = False
-
-        self.relu256_128_catu = nn.ReLU(inplace=True)
-        self.up128 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.conv_512_256 = nn.Conv2d(512, 256, 3, 1, 1)
+        self.bn_512_256 = nn.BatchNorm2d(256, track_running_stats=True)
+        self.bn_512_256.training = False
+        self.relu512_256 = nn.ReLU(inplace=True)
 
 
 
+        self.conv_512_256_n = nn.Conv2d(512, 256, 3, 1, 1)
+        self.bn_512_256_n = nn.BatchNorm2d(256, track_running_stats=True)
+        self.bn_512_256_n.training = False
+        self.relu512_256_n = nn.ReLU(inplace=True)
 
-        self.conv256_128_u = nn.Conv2d(256, 128, 3, 1, 1)
-        self.bn256_128_u = nn.BatchNorm2d(128, track_running_stats=True)
-        self.bn256_128_u.training = False
-
-        self.relu256_128_u = nn.ReLU(inplace=True)
-        self.conv128_64_u = nn.Conv2d(128, 64, 3, 1, 1)
-        self.bn128_64_u = nn.BatchNorm2d(64, track_running_stats=True)
-        self.bn128_64_u.training = False
-
-        self.relu128_64_u = nn.ReLU(inplace=True)
-        self.up64 = nn.Upsample(scale_factor=2, mode='nearest')
+        self.conv_256_128 = nn.Conv2d(256, 128, 3, 1, 1)
+        self.bn_256_128 = nn.BatchNorm2d(128, track_running_stats=True)
+        self.bn_256_128.training = False
+        self.relu256_128 = nn.ReLU(inplace=True)
 
 
-        self.conv128_64_ucat = nn.Conv2d(128, 64, 3, 1, 1)
-        self.bn128_64_ucat = nn.BatchNorm2d(64, track_running_stats=True)
-        self.bn128_64_ucat.training = False
+        self.conv_256_128_n = nn.Conv2d(256, 128, 3, 1, 1)
+        self.bn_256_128_n = nn.BatchNorm2d(128, track_running_stats=True)
+        self.bn_256_128_n.training = False
+        self.relu256_128_n = nn.ReLU(inplace=True)
 
-        self.relu128_64_ucat = nn.ReLU(inplace=False)
-        self.conv64_64_ucat = nn.Conv2d(64, 64, 3, 1, 1)
-        self.bn64_64_ucat = nn.BatchNorm2d(64, track_running_stats=True)
-        self.bn64_64_ucat.training = False
+        self.conv_128_64 = nn.Conv2d(128, 64, 3, 1, 1)
+        self.bn_128_64 = nn.BatchNorm2d(64, track_running_stats=True)
+        self.bn_128_64.training = False
+        self.relu128_64 = nn.ReLU(inplace=True)
 
-        self.relu64_64_u = nn.ReLU(inplace=True)
+
+        self.conv_128_64_n = nn.Conv2d(128, 64, 3, 1, 1)
+        self.bn_128_64_n = nn.BatchNorm2d(64, track_running_stats=True)
+        self.bn_128_64_n.training = False
+        self.relu128_64_n = nn.ReLU(inplace=True)
+
+        self.conv_64_64 = nn.Conv2d(64, 64, 3, 1, 1)
+        self.bn_64_64 = nn.BatchNorm2d(64, track_running_stats=True)
+        self.bn_64_64.training = False
+        self.relu64_64 = nn.ReLU(inplace=True)
+
 
         self.conv_out_64 = nn.Conv2d(64, 14, 1, 1, 0)
-        
-        '''
 
 
     def copy_bn_layer(self, pytorch_bn_layer, torch_bn_layer):
@@ -346,111 +283,63 @@ class UNetRGBD(nn.Module):
         self.copy_conv_layer(self.conv512_1024, self.fifth_block.get(2))
         self.copy_conv_layer(self.conv1024_512, self.fifth_block.get(3))
 
-
-        self.make_everything_identity()
-
-
-        print(self.lua_unet)
-
-        # self.sixth_block = self.lua_unet.get(1).get(1).get(2).get(1).get(2).get(1).get(3)
-        #
-        #
-        # self.seventh_block = self.lua_unet.get(1).get(1).get(2).get(1).get(5)
-        #
-        #
-        # self.eigth_block = self.lua_unet.get(1).get(1).get(5)
-        #
-        #
-        # self.ninth_block = self.lua_unet.get(4)
-        #
-        #
-        # self.tenth_block = self.lua_unet.get(5)
+        # self.remove_modules()
 
 
-        '''
-        self.third_block = self.lua_unet.get(1).get(1).get(2).get(1).get(1)
+        self.sixth_block = self.lua_unet.get(1).get(1).get(2).get(1).get(2).get(1).get(3)
 
-        self.copy_conv_layer(self.conv128_256, self.third_block.get(0))
-        self.copy_bn_layer(self.bn128_256, self.third_block.get(1))
-        self.copy_conv_layer(self.conv256_256, self.third_block.get(3))
-        self.copy_bn_layer(self.bn256_256, self.third_block.get(4))
+        self.copy_conv_layer(self.conv_512_512, self.sixth_block.get(0))
+        self.copy_bn_layer(self.bn_512_512, self.sixth_block.get(1))
+        self.copy_conv_layer(self.conv_512_256, self.sixth_block.get(3))
+        self.copy_bn_layer(self.bn_512_256, self.sixth_block.get(4))
 
-        # block = unet:get(2):get(2):get(3):get(2):get(3):get(2):get(2)
-        # save_conv_unet_block(block)
+        self.seventh_block = self.lua_unet.get(1).get(1).get(2).get(1).get(5)
 
-        self.fourth_block = self.lua_unet.get(1).get(1).get(2).get(1).get(2).get(1).get(1)
+        self.copy_conv_layer(self.conv_512_256, self.seventh_block.get(0))
+        self.copy_bn_layer(self.bn_512_256, self.seventh_block.get(1))
+        self.copy_conv_layer(self.conv_256_128, self.seventh_block.get(3))
+        self.copy_bn_layer(self.bn_256_128, self.seventh_block.get(4))
 
-        self.copy_conv_layer(self.conv256_512, self.fourth_block.get(0))
-        self.copy_bn_layer(self.bn256_512, self.fourth_block.get(1))
-        self.copy_conv_layer(self.conv512_512, self.fourth_block.get(3))
-        self.copy_bn_layer(self.bn512_512, self.fourth_block.get(4))
+        self.eigth_block = self.lua_unet.get(1).get(1).get(5)
 
-        self.fifth_block = self.lua_unet.get(1).get(1).get(2).get(1).get(2).get(1).get(2).get(1)
-        self.copy_conv_layer(self.conv512_1024, self.fifth_block.get(1))
-        self.copy_conv_layer(self.conv1024_512, self.fifth_block.get(2))
-
-        self.sixth_block = self.lua_unet.get(1).get(1).get(2).get(1).get(2).get(1).get(4)
-        self.copy_conv_layer(self.conv1024_512_u, self.sixth_block.get(0))
-        self.copy_bn_layer(self.bn1024_512_u, self.sixth_block.get(1))
-        self.copy_conv_layer(self.conv512_256_u, self.sixth_block.get(3))
-        self.copy_bn_layer(self.bn512_256_u, self.sixth_block.get(4))
-
-        self.seventh_block = self.lua_unet.get(1).get(1).get(2).get(1).get(4)
-        print(self.seventh_block)
-        self.copy_conv_layer(self.conv512_256_catu, self.seventh_block.get(0))
-        self.copy_bn_layer(self.bn512_256_catu, self.seventh_block.get(1))
-        self.copy_conv_layer(self.conv256_128_catu, self.seventh_block.get(3))
-        self.copy_bn_layer(self.bn256_128_catu, self.seventh_block.get(4))
+        self.copy_conv_layer(self.conv_256_128_n, self.eigth_block.get(0))
+        self.copy_bn_layer(self.bn_256_128_n, self.eigth_block.get(1))
+        self.copy_conv_layer(self.conv_128_64, self.eigth_block.get(3))
+        self.copy_bn_layer(self.bn_128_64, self.eigth_block.get(4))
 
 
-        # block = unet:get(2):get(2):get(5)
-        # save_conv_unet_block(block)
+        self.ninth_block = self.lua_unet.get(4)
 
-        self.eigth_block = self.lua_unet.get(1).get(1).get(4)
-        self.copy_conv_layer(self.conv256_128_u, self.eigth_block.get(0))
-        self.copy_bn_layer(self.bn256_128_u, self.eigth_block.get(1))
-        self.copy_conv_layer(self.conv128_64_u, self.eigth_block.get(3))
-        self.copy_bn_layer(self.bn128_64_u, self.eigth_block.get(4))
+        self.copy_conv_layer(self.conv_128_64_n, self.ninth_block.get(0))
+        self.copy_bn_layer(self.bn_128_64_n, self.ninth_block.get(1))
+        self.copy_conv_layer(self.conv_64_64, self.ninth_block.get(3))
+        self.copy_bn_layer(self.bn_64_64, self.ninth_block.get(4))
 
-        # block = unet:get(4)
-        # save_conv_unet_block(block)
-
-        self.ninth_block = self.lua_unet.get(3)
-        self.copy_conv_layer(self.conv128_64_ucat, self.ninth_block.get(0))
-        self.copy_bn_layer(self.bn128_64_ucat, self.ninth_block.get(1))
-        self.copy_conv_layer(self.conv64_64_ucat, self.ninth_block.get(3))
-        self.copy_bn_layer(self.bn64_64_ucat, self.ninth_block.get(4))
-
-        # block = unet:get(5)
-        # save_conv(block)
-
-        self.tenth_block = self.lua_unet.get(4)
+        self.tenth_block = self.lua_unet.get(5)
         self.copy_conv_layer(self.conv_out_64, self.tenth_block)
-        '''
-
 
         print('Have copied the weights of the first block')
 
 
     def make_everything_identity(self):
 
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[2].modules[1].modules[3] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[2].modules[1].modules[4] = torch.legacy.nn.Identity()
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[2].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[2].modules[1].modules[3]
 
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[5] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[3] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[4] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[6] = torch.legacy.nn.Identity()
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[2].modules[1].modules[3]
 
-        self.lua_unet.modules[1].modules[1].modules[5] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[3] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[4] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[1].modules[1].modules[6] = torch.legacy.nn.Identity()
+        del self.lua_unet.modules[1].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[3]
+        del self.lua_unet.modules[1].modules[1].modules[3]
 
-        self.lua_unet.modules[2] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[3] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[4] = torch.legacy.nn.Identity()
-        self.lua_unet.modules[5] = torch.legacy.nn.Identity()
+        del self.lua_unet.modules[2]
+        del self.lua_unet.modules[2]
+        del self.lua_unet.modules[2]
+        del self.lua_unet.modules[2]
 
     def run_torch_pytorch_import_test(self):
 
@@ -510,13 +399,13 @@ class UNetRGBD(nn.Module):
         #
         # yTorch = self.tenth_block.forward(yTorch)
 
-        # print('yTorch shape = ', yTorch_out.detach().numpy().shape)
-        print('yTorch shape = ', np.array(yTorch_out).shape)
+        print('yTorch shape = ', yTorch_out.detach().numpy().shape)
+        # print('yTorch shape = ', np.array(yTorch_out).shape)
 
         # ypyTorch_rgb, ypyTorch_d = self.forward((myrgbImg, mydImg))
         ypyTorch_out = self.forward((myrgbImg, mydImg))
 
-        # print('ypTorch_rgb shape = ', ypyTorch_rgb.detach().numpy().shape)
+        print('ypTorch_rgb shape = ', ypyTorch_out.detach().numpy().shape)
         # print('ypTorch_depth shape = ', ypyTorch_d.detach().numpy().shape)
 
         print('DIFF {}'.format(np.sum(yTorch_out.detach().numpy() - ypyTorch_out.detach().numpy())))
@@ -629,85 +518,49 @@ class UNetRGBD(nn.Module):
         out = self.conv1024_512(out)
         out = self.up(out)
 
-        # out = self.conv_512_512(out)
-        # out = self.bn_512_512(out)
-        # out = self.relu_512_512(out)
-        #
-        # out = self.conv_512_256(out)
-        # out = self.bn_512_256(out)
-        # out = self.relu_512_256(out)
-        #
-        # out = self.up256(out)
+
+        out = self.conv_512_512(out)
+        out = self.bn_512_512(out)
+        out = self.relu_512_512(out)
+
+        out = self.conv_512_256(out)
+        out = self.bn_512_256(out)
+        out = self.relu_512_256(out)
+
+        out = self.up(out)
 
 
+        out = torch.cat([out_rgb_relu128, out_d_relu128, out], dim=1)
+        out = self.conv_512_256_n(out)
+        out = self.bn_512_256_n(out)
+        out = self.relu512_256_n(out)
 
-        '''
-        out = self.conv64_128(out)
-        out = self.bn64_128(out)
-        out = self.relu64_128(out)
-        out = self.conv128_128(out)
-        out = self.bn128_128(out)
-        out_relu128 = self.relu128_128(out)
+        out = self.conv_256_128(out)
+        out = self.bn_256_128(out)
+        out = self.relu256_128(out)
 
-        out = self.pool_128(out_relu128)
+        out = self.up(out)
 
-        out = self.conv128_256(out)
-        out = self.bn128_256(out)
-        out = self.relu128_256(out)
-        out = self.conv256_256(out)
-        out = self.bn256_256(out)
-        out_relu256 = self.relu256_256(out)
+        out = torch.cat([out_rgb_relu64, out_d_relu64, out], dim=1)
+        out = self.conv_256_128_n(out)
+        out = self.bn_256_128_n(out)
+        out = self.relu256_128_n(out)
 
-        out = self.pool_256(out_relu256)
+        out = self.conv_128_64(out)
+        out = self.bn_128_64(out)
+        out = self.relu128_64(out)
 
-        out = self.conv256_512(out)
-        out = self.bn256_512(out)
-        out = self.relu256_512(out)
-        out = self.conv512_512(out)
-        out = self.bn512_512(out)
-        out_relu512 = self.relu512_512(out)
+        out = self.up(out)
 
-        out = self.pool_512(out_relu512)
-        out = self.conv512_1024(out)
-        out = self.conv1024_512(out)
-        out = self.up512(out)
+        out = torch.cat([out_rgb_relu32, out_d_relu32, out], dim=1)
+        out = self.conv_128_64_n(out)
+        out = self.bn_128_64_n(out)
+        out = self.relu128_64_n(out)
 
-        out = torch.cat([out_relu512, out], dim=1)
-        out = self.conv1024_512_u(out)
-        out = self.bn1024_512_u(out)
-        out = self.relu1024_512_u(out)
-        out = self.conv512_256_u(out)
-        out = self.bn512_256_u(out)
-        out = self.relu512_256_u(out)
-        out = self.up256(out)
-
-        out = torch.cat([out_relu256, out], dim=1)
-        out = self.conv512_256_catu(out)
-        out = self.bn512_256_catu(out)
-        out = self.relu512_256_catu(out)
-        out = self.conv256_128_catu(out)
-        out = self.bn256_128_catu(out)
-        out = self.relu256_128_catu(out)
-        out = self.up128(out)
-
-        out = torch.cat([out_relu128, out], dim=1)
-        out = self.conv256_128_u(out)
-        out = self.bn256_128_u(out)
-        out = self.relu256_128_u(out)
-        out = self.conv128_64_u(out)
-        out = self.bn128_64_u(out)
-        out = self.relu128_64_u(out)
-        out = self.up64(out)
-
-        out = torch.cat([out_relu64, out], dim=1)
-        out = self.conv128_64_ucat(out)
-        out = self.bn128_64_ucat(out)
-        out = self.relu128_64_ucat(out)
-        out = self.conv64_64_ucat(out)
-        out = self.bn64_64_ucat(out)
-        out = self.relu64_64_u(out)
+        out = self.conv_64_64(out)
+        out = self.bn_64_64(out)
+        out = self.relu64_64(out)
 
         out = self.conv_out_64(out)
-        '''
 
-        return out #(out_rgb, out_d)
+        return out
